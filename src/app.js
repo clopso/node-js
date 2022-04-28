d3472f0919b72e8a5b1b5c2c8c304b7cb5bf0c26
clopso <caiolemes306@gmail.com>
2022-04-28 15:07:38 -0300

    post json (03)

--- src/app.js
+++ src/app.js
@@ -1,18 +1,23 @@
-const express = require('express');
-const { json } = require('body-parser');
-const cors = require('cors');
+const express = require('express')
+const { json } = require('body-parser')
+const cors = require('cors')
 
-const server = express();
-const port = 3999;
+const server = express()
+const port = 3001
 
-server.use(json());
-server.use(cors);
+server.use(json())
+server.use(cors())
 
 server.get('/', (req, res) =>{
-    res.send("seja bem vindo :)");
+    res.send("seja bem vindo")
 })
 
+server.post('/', (req, res) =>{
+    const resquest = req.body
+    console.log(resquest.nomes)
+    res.status(201).send()
+})
 
 server.listen(port, (req, res) => {
-    console.log(`rodando na porta ${port}`);
+    console.log(`rodando na porta ${port}`)
 })
