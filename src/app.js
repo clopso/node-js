3aaf10961ae8bc781f88677dfbf25606abbbb5b7
clopso <caiolemes306@gmail.com>
2022-04-28 15:20:12 -0300

    get data e delete (04)

--- src/app.js
+++ src/app.js
@@ -8,14 +8,22 @@ const port = 3001
 server.use(json())
 server.use(cors())
 
+let nomes = [];
+
 server.get('/', (req, res) =>{
-    res.send("seja bem vindo")
+    res.status(200).send(nomes)
 })
 
 server.post('/', (req, res) =>{
     const resquest = req.body
-    console.log(resquest.nomes)
+    console.log(resquest.nome)
+    nomes.push(resquest.nome)
     res.status(201).send()
+})
+
+server.delete('/', (req, res) => {
+    nomes = [];
+    res.status(202).send()
 })
 
 server.listen(port, (req, res) => {
